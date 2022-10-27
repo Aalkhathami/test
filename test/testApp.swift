b44f2d18657b5fdb409b88399c6eaa70d8575faa
amal <agalkhathami22@twq.idserve.net>
2022-10-27 11:31:42 +0300

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by amalghurm on 02/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
